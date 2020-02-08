a4a621fd1e3b82ffb277b2c06630e0f79e2ec453
Niklas Ståhlenberg <Stais3r@gmail.com>
2020-02-08 13:30:20 +0100

    Add project files.

---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+document.getElementById('colorGray').onclick = changeColor;
+document.getElementById('reset').onclick = resetColor;
+
+function changeColor() { 
+    document.getElementsByTagName('body')[0].style.backgroundColor = 'gray';
+    document.getElementsByTagName('body')[0].style.color = 'white';        
+}
+
+function resetColor() {
+    document.getElementsByTagName('body')[0].style.backgroundColor = 'azure';
+    document.getElementsByTagName('body')[0].style.color = 'black';
+}
